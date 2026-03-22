163de951b6de20936b5cbe2ff98522665aa9bd7b
nroudeau <niels.roudeau@gmail.com>
2026-03-22 19:47:09 +0100

    removed init.py comments

--- peptide_pipeline/__init__.py
+++ peptide_pipeline/__init__.py
@@ -1,3 +1,1 @@
-# Peptide Pipeline Package
-# This package contains the multi-expert peptide generation pipeline modules.
-# from orchestrator import run callo so main and others can do from peptide_pipeline import run(orchestrator) (level separation)
+
